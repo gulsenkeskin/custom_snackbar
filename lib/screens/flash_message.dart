ff94e10e62d1e29c84beeb285b55d72ec5b220f6
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-23 07:27:54 +0300

    Update flash_message.dart

--- lib/screens/flash_message.dart
+++ lib/screens/flash_message.dart
@@ -1,3 +1,4 @@
+import 'package:custom_snackbar/screens/custom_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -15,88 +16,14 @@ class _FlashMessageScreenState extends State<FlashMessageScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
               behavior: SnackBarBehavior
                   .floating, //ekranla snackbar arasına margin ekler
               backgroundColor: Colors.transparent,
               elevation: 0,
-              content: Stack(
-                alignment: Alignment.center,
-                clipBehavior: Clip.none,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(16),
-                    height: 90,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFFC72C41),
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child: Row(
-                      children: [
-                        const SizedBox(
-                          width: 48,
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                "Mesaj mesaj!",
-                                style: TextStyle(
-                                    fontSize: 18, color: Colors.white),
-                              ),
-                              Text(
-                                "Bu email adresi kullanılıyor. Lütfen tekrar deneyin.Bu email adresi kullanılıyor. Lütfen tekrar deneyin",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 12),
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                      ),
-                      child: Stack(
-                        children: [
-                          SvgPicture.asset(
-                            "assets/icons/bubbles.svg",
-                            height: 48,
-                            width: 48,
-                            color: const Color(0xFFB01336),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                      top: -20,
-                      left: 0,
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          SvgPicture.asset(
-                            "assets/icons/chat.svg",
-                            height: 40,
-                            color: const Color(0xFFB01336),
-                          ),
-                          Positioned(
-                            child: SvgPicture.asset("assets/icons/fail.svg",
-                                height: 40, color: Colors.white),
-                            top: 10,
-                            height: 16,
-                          )
-                        ],
-                      ))
-                ],
+              content: CustomSnackBar(
+                errorText:
+                    "Bu email adresi kullanılıyor. Lütfen tekrar deneyin.Bu email adresi kullanılıyor. Lütfen tekrar deneyin",
               ),
             ));
           },
