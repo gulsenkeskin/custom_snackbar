a882744174a803f0f62a9e404f4f87a1a30937be
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-23 06:35:28 +0300

    Create flash_message.dart

---
+++ lib/screens/flash_message.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FlashMessageScreen extends StatefulWidget {
+  const FlashMessageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FlashMessageScreen> createState() => _FlashMessageScreenState();
+}
+
+class _FlashMessageScreenState extends State<FlashMessageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
