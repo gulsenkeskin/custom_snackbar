978e21a2e6f9fbf5740a216f2937278be737f402
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-23 06:47:43 +0300

    removed background

--- lib/screens/flash_message.dart
+++ lib/screens/flash_message.dart
@@ -13,21 +13,22 @@ class _FlashMessageScreenState extends State<FlashMessageScreen> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: (){
-            ScaffoldMessenger.of(context).showSnackBar(
-               SnackBar(
-                  behavior:  SnackBarBehavior.floating,//ekranla snackbar arasına margin ekler
-                  content: Container(
-                    padding: const EdgeInsets.all(16),
-                    height: 90,
-                    decoration: const BoxDecoration(
-                      color: Color(0xFFC72C41),
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child:const Text("Bu email adresi kullanılıyor. Lütfen tekrar deneyin"),
-                  )
-              )
-            );
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                behavior: SnackBarBehavior
+                    .floating, //ekranla snackbar arasına margin ekler
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                content: Container(
+                  padding: const EdgeInsets.all(16),
+                  height: 90,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFC72C41),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: const Text(
+                      "Bu email adresi kullanılıyor. Lütfen tekrar deneyin"),
+                )));
           },
           child: const Text("Mesaj Göster"),
         ),
