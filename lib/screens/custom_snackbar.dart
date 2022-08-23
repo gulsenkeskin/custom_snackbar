420b1e5b557038090bf8719d99d927a6125f5740
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-23 07:27:52 +0300

    Create custom_snackbar.dart

---
+++ lib/screens/custom_snackbar.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomSnackBar extends StatelessWidget {
+  const CustomSnackBar({Key? key, required this.errorText}) : super(key: key);
+
+  final String errorText;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          height: 90,
+          decoration: const BoxDecoration(
+            color: Color(0xFFC72C41),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 48,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:  [
+                    const Text(
+                      "Mesaj mesaj!",
+                      style: TextStyle(
+                          fontSize: 18, color: Colors.white),
+                    ),
+                    const Spacer(),
+                    Text(
+                     errorText,
+                      style: TextStyle(
+                          color: Colors.white, fontSize: 12),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+            ),
+            child: Stack(
+              children: [
+                SvgPicture.asset(
+                  "assets/icons/bubbles.svg",
+                  height: 48,
+                  width: 48,
+                  color: const Color(0xFFB01336),
+                )
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+            top: -20,
+            left: 0,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  "assets/icons/chat.svg",
+                  height: 40,
+                  color: const Color(0xFFB01336),
+                ),
+                Positioned(
+                  child: SvgPicture.asset("assets/icons/fail.svg",
+                      height: 40, color: Colors.white),
+                  top: 10,
+                  height: 16,
+                )
+              ],
+            ))
+      ],
+    );
+  }
+}
